e18eb88376b0132f8dec7ea3a1361786a48dcab7
git config --global user.name irina-moshkovskaya <iryna2005m30@gmail.com>
2017-12-03 19:36:32 +0200

    Initial commit. Added first greeting form.

---
+++ WpfApp2/MainMenu.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        public MainMenu()
+        {
+            
+            InitializeComponent();
+            this.Loaded += OnLoad;
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav;
+            nav = NavigationService.GetNavigationService(this);
+            if (nav.CanGoBack)
+            {
+                nav.RemoveBackEntry();
+            }
+           
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines;
+            lines = File.ReadAllLines("Results.txt");
+
+            if (lines.Length == 0)
+                MessageBox.Show("Нікому ще не вдавалося перемогти.\nВи можете бути першими!", "Повідомлення");
+            else
+            {
+                NavigationService nav;
+                nav = NavigationService.GetNavigationService(this);
+            }
+
+        }
+
+        private void button_NewGame_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav;
+            nav = NavigationService.GetNavigationService(this);
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Application app = Application.Current;
+            app.Shutdown();
+        }
+    }
+}
